4626143080361f677c4651c364f85ba84066669d
Ilnar.Kadyirov <kadyyr@yandex.ru>
2023-05-01 10:59:04 +0300

    corrected remarks HW #9

--- src/test/java/testsHomeWork9/ParamTest.java
+++ src/test/java/testsHomeWork9/ParamTest.java
@@ -10,32 +10,21 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
-
 import java.util.List;
 import java.util.stream.Stream;
-
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
-
     public class ParamTest {
-
         @BeforeEach
         void setUp() {
-
             Configuration.baseUrl = "http://mirinstrumenta63.ru";
         }
-
         @ValueSource(strings= {
-                "Контакты",
-                "Доставка",
-                "Оплата",
-                "Возврат товара"
-        }
-        )
+                "Контакты", "Доставка", "Оплата", "Возврат товара"} )
+
         @ParameterizedTest(name = "тестирование раздела {0}")
         void headButtonsTest( String razdel) {
-
             Selenide.open("");
             $$x("//ul//li//a").findBy(Condition.text(razdel)).click();
             $("h1").shouldHave(Condition.text(razdel));
@@ -44,31 +33,24 @@ import static com.codeborne.selenide.Selenide.*;
             return Stream.of(
                     Arguments.of(List.of("Сварочная проволока", "Сварочные аппараты",
                             "Сварочные электроды", "Сварочные маски",
-                            "Сварочные принадлежности","Аппараты для сварки ПВХ труб"))
-            );
+                            "Сварочные принадлежности","Аппараты для сварки ПВХ труб")));
         }
         @MethodSource
         @ParameterizedTest(name = "тестирование отображения раздела в боковом меню")
         void sideMenuTest(List<String> result) {
-
             Selenide.open("/category/svarochnoe-oborudovanie/");
             $$(".js-category-link").filter(visible)
-               .shouldHave(CollectionCondition.texts(result));
-
+            .shouldHave(CollectionCondition.texts(result));
         }
         @CsvSource (value = {
                 "Электродрель, Поиск по запросу Электродрель",
-                "Лестницы,Поиск по запросу Лестницы"
-        }
-        )
+                "Лестницы,Поиск по запросу Лестницы"} )
+
         @ParameterizedTest(name = "тестирование выдачи при поиске: {0} ")
         void searchTest (String razdel, String result) {
-             Selenide.open("");
+            Selenide.open("");
             $("[placeholder='Поиск по каталогу']").setValue(razdel).pressEnter();
             $("h1").shouldHave(Condition.text(result));
        }
-
-
-
     }
 
